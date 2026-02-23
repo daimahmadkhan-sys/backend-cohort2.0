e281e14891908058ef07a19f55f3cfa4d0a31f6b
Daim Khan <daimahmadkhan@gmail.com>
2026-02-23 16:06:45 +0530

    Added Day-7 CRUD OP

---
+++ Day-7/src/config/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+
+const mongoose = require("mongoose")
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("Connected to Database");
+        
+    })
+}
+
+module.exports = connectToDB
